ad5887ab7d3760403fb48e944e36faee763815da
Santiago Grillo <admsanti90@gmail.com>
2024-10-09 10:55:50 -0300

    Arreglo diseño de botón "Nuevo usuario"

--- src/resources/views/roles/index.blade.php
+++ src/resources/views/roles/index.blade.php
@@ -10,7 +10,7 @@
                     <div class="mt-4">
                         @can('role-create')
                             <a href="{{ route('roles.create') }}"
-                                class="inline-flex items-center px-4 py-2 mb-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-700 active:bg-indigo-900 focus:outline-none focus:border-indigo-900 focus:ring ring-indigo-300 disabled:opacity-25 transition ease-in-out duration-150 float-end">
+                                class="inline-flex items-center px-4 py-2 mb-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-700 active:bg-indigo-900 focus:outline-none focus:border-indigo-900 focus:ring ring-indigo-300 disabled:opacity-25 transition ease-in-out duration-150">
                                 Nuevo rol
                             </a>
                         @endcan
